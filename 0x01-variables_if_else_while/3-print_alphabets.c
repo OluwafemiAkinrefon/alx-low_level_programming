146a3f7758e2f343676b93ba559b104a086f8daf
OluwafemiAkinrefon <akinrefonoluwafemi@gmail.com>
2023-10-09 21:22:25 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <sdio.h>
+/**
+ * main - code starts here
+ * Return: everytime 0 (success)
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c<= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
