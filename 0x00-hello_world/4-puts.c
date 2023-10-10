eb91093886f9d59f485429c2f265e9740ac15b70
OluwafemiAkinrefon <akinrefonoluwafemi@gmail.com>
2023-10-09 18:45:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-This program prints a line using the put function
+ * return:0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,");
+return (0);
+}
