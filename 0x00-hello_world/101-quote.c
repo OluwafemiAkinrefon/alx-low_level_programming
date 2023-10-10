d1143f35d932939371ab6e30c03d14f743d63b03
OluwafemiAkinrefon <akinrefonoluwafemi@gmail.com>
2023-10-09 20:17:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+#include<unistd.h>
+/**
+ * main - this is a program that helps to print a line to the standard error
+ * retun: 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
