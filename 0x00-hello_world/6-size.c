33a37701adbe3efcbcb4751fc9e9ec0feaa4ba3b
OluwafemiAkinrefon <akinrefonoluwafemi@gmail.com>
2023-10-09 20:04:12 -0700

    Intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that helps printing the size of a computer types
+ * return: 0 (success)
+ */
+int main(void)
+	char o;
+	int p;
+	long int q;
+	long long int r;
+	float s;
+printf("size of a char %lubyte(s)\n",(
