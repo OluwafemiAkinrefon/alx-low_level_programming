2c1f8dcd633ce5d9b04654fc1f06cd6c238975f1
OluwafemiAkinrefon <akinrefonoluwafemi@gmail.com>
2023-10-17 18:57:30 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * reset_to_98 - here is the function name
  * @n: this function has the parameter
+ * @n: input
+ * Return: n
  */
-
 void reset_to_98(int *n)
 {
-
 	*n = 98;
 }
