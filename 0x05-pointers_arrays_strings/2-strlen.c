3ec5349ded343591ff3e12e04611f8ef6c2b3adc
OluwafemiAkinrefon <akinrefonoluwafemi@gmail.com>
2023-10-17 19:38:54 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - length of the string will be returned
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s l = '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
+
